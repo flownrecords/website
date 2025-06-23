061980bfc889823b59bc9510afadcc1dbd36fd42
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2025-06-23 19:52:40 +0100

    💄 Added styling for tool page and about

--- src/pages/general/AboutUs.tsx
+++ src/pages/general/AboutUs.tsx
@@ -4,72 +4,50 @@ export default function AboutUs() {
     const Team = [
         {
             id: 1,
-            name: "Jurgen",
-            role: "Founder & Developer",
-            image: "https://avatars.githubusercontent.com/u/1006260?v=4"
+            name: "Jürgen",
+            role: "Lead Developer",
+            image: "https://placehold.co/256x256"
         },
-        {
-            id: 2,
-            name: "John Doe",
-            role: "Designer",
-            image: "https://via.placeholder.com/150"
-        },
-        {
-            id: 3,
-            name: "Jane Smith",
-            role: "Product Manager",
-            image: "https://via.placeholder.com/150"
-        }
     ];
 
     return (
         <>
-            <Splash/>
+            <Splash uppertext="About"/>
 
-            <div className="container mx-auto max-w-5xl">
-                <div className="px-6 md:px-10 mx-auto">
-                    <h1 className="text-4xl font-bold mt-2">Our Mission</h1>
-                    <p className="text-white/75">
-                        Flown Records is a platform that aims to provide a comprehensive and
-                        user-friendly experience for pilots and aviation enthusiasts. Our
-                        mission is to simplify the process of tracking and managing flight
-                        records, making it easier for users to access their flight data and
-                        share it with others. We strive to create a community-driven platform
-                        that fosters collaboration and knowledge sharing among aviation
-                        professionals and enthusiasts alike.
-                    </p>
+            <div className="container mx-auto max-w-6xl p-4 lg:px-0">
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+                    <div className="">
+                        <h1 className="text-4xl font-bold">Our Mission</h1>
+                        <p className="text-white/75">
+                            Placeholder
+                        </p>
+                    </div>
+
+                    <div className="">
+                        <h1 className="text-4xl font-bold">Our History</h1>
+                        <p className="text-white/75">
+                            Placeholder
+                        </p>
+                    </div>
                 </div>
 
-                <div className="px-6 md:px-10 mx-auto mt-4">
-                    <h1 className="text-4xl font-bold mt-2 text-right">Our History</h1>
-                    <p className="text-white/75 text-right">
-                        Flown Records was founded in 2023 by a group of aviation enthusiasts who
-                        recognized the need for a more efficient and user-friendly way to manage
-                        flight records. With a passion for aviation and a commitment to
-                        innovation, our team set out to create a platform that would
-                        revolutionize the way pilots and aviation enthusiasts track their
-                        flights. Since then, we have been dedicated to continuously improving
-                        our platform and expanding our features to better serve our users.
-                    </p>
-                </div>
-
-                <div className="px-6 md:px-10 mx-auto mt-4">
+                <div className="mt-4">
                     <h1 className="text-4xl font-bold mt-2">Our Team</h1>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-4 mb-6">
+                    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-2">
                         {Team.map(member => (
                             <div
                                 key={member.id}
-                                className="relative bg-gradient-to-br to-neutral-900 from-neutral-800 rounded-lg p-6 text-white overflow-hidden ring-2 ring-white/50 text-center"
+                                className="relative bg-gradient-to-br to-neutral-900 from-neutral-800 rounded-lg p-6 text-white overflow-hidden ring-2 ring-white/25 text-center"
                             >
                                 <img
                                     src={member.image}
                                     alt={member.name}
-                                    className="w-24 h-24 rounded-full inline-block"
+                                    className="w-24 h-24 rounded-full inline-block ring-2 ring-white/25"
                                 />
-                                <h1 className="mt-2">
+                                <h1 className="mt-2 text-lg font-semibold">
                                     {member.name}
                                 </h1>
-                                <p className="text-white/50">
+                                <p className="mt-4 text-sm ring-1 ring-second-accent bg-second-accent/25 py-0.5 px-2 rounded-xl mx-4">
                                     {member.role}
                                 </p>
                             </div>
--- src/pages/tools/Tools.tsx
+++ src/pages/tools/Tools.tsx
@@ -1,9 +1,28 @@
+import { Link } from "react-router-dom"
+import Splash from "../../components/general/Splash";
+
 export default function Tools() {
     return (
-        <div>
-        <h1>Tools</h1>
-        <p>Welcome to the tools page!</p>
-        {/* Add more tools-related content here */}
-        </div>
+        <>
+            <Splash uppertext="Tools"/>
+
+            <div className="container mx-auto max-w-6xl p-4 lg:px-0">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+                    <Link to="/" className="rounded-lg overflow-hidden ring-2 ring-white/25 hover:opacity-75 transition-all duration-150">
+                        <img src="https://placehold.co/1920x720" className="lg:max-h-64"/>
+                        <div className="p-4">
+                            <h1 className="font-semibold text-white text-xl flex items-center">
+                                Flight Planner <span className="text-xs ring-1 ring-accent bg-accent/25 py-0.5 px-2 rounded-xl ml-2">WIP</span>
+                            </h1>
+                            <p className="text-white/50 text-md">
+                                A tool to help you plan your flights, including route planning, fuel calculations, and more.
+                            </p>
+                        </div>
+                    </Link>
+
+                </div>
+            </div>
+        </>
     );
 }
