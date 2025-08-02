77d876600a2413834eddcab1a2765da9e302bf0d
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2025-08-02 19:02:07 +0100

    ✨ Enhance Button component to support React nodes as text and add disabled state handling

--- src/components/general/Button.tsx
+++ src/components/general/Button.tsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
 
 type ButtonProps = {
-    text?: string;
+    text?: string | React.ReactNode;
     to?: string;
     className?: string;
     onClick?: React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
     type?: "button" | "submit" | "reset";
     styleType?: "normal" | "small";
+    disabled?: boolean;
 };
 
+function alertIfDisabled() {
+    console.warn("Button is disabled, no action will be performed.");
+}
+
 const Button: React.FC<ButtonProps> = ({
+    disabled = false,
     text = "Click me",
     to,
     className = "",
@@ -18,25 +24,30 @@ const Button: React.FC<ButtonProps> = ({
     styleType = "normal",
 }) => {
     const style = `
+    ${disabled ? "opacity-50" : "hover:opacity-75"}
+    ${disabled ? "cursor-default" : "cursor-pointer"}
+
     ${className}
+
     ${styleType === "small" ? "text-sm" : "text-base"}
     ${styleType === "small" ? "py-1" : ""}
+
     inline-block
-    cursor-pointer bg-gradient-to-t 
+    bg-gradient-to-t 
     from-neutral-900 to-neutral-800 
-    hover:opacity-75 transition duration-150
+    transition duration-150
     text-white
     py-1 md:py-2 px-4 md:px-6 
     rounded-lg text-center
     ring-2 ring-white/25 
   `;
 
-    return to ? (
+    return to && !disabled ? (
         <Link to={to} onClick={onClick} className={style}>
             {text}
         </Link>
     ) : (
-        <button onClick={onClick} className={style} type={type}>
+        <button onClick={disabled ? alertIfDisabled : onClick} className={style} type={type}>
             {text}
         </button>
     );
