2648b315fe917ea92c02b37ec47ec27c0281a4a4
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2025-09-02 12:17:33 +0100

    ✨ Add new guides with images for flight data upload steps in Guides component

--- src/pages/general/Guides.tsx
+++ src/pages/general/Guides.tsx
@@ -6,6 +6,14 @@ import Step1Guide1 from "../../assets/guides/guide_11.jpg";
 import Step2Guide1 from "../../assets/guides/guide_12.jpg";
 import Step3Guide1 from "../../assets/guides/guide_13.jpg";
 import Step4Guide1 from "../../assets/guides/guide_14.jpg";
+
+import Step1Guide2 from "../../assets/guides/guide_21.jpg";
+import Step2Guide2 from "../../assets/guides/guide_22.jpg";
+import Step3Guide2 from "../../assets/guides/guide_23.jpg";
+import Step4Guide2 from "../../assets/guides/guide_24.jpg";
+import Step5Guide2 from "../../assets/guides/guide_25.jpg";
+import Step6Guide2 from "../../assets/guides/guide_26.jpg";
+
 import { Link } from "react-router-dom";
 import Footer from "../../components/general/Footer";
 
@@ -55,7 +63,40 @@ export default function Guides() {
         {
             id: "airnavradar",
             title: "How to upload flight data from AirNav Radar to a logbook entry",
-            content: <><span className="text-white/25">No steps available for this guide.</span></>
+            content: <>
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                    <div className={"flex flex-col " + stepStyle}>
+                        <h3 className="text-lg font-semibold text-white">Step 1</h3>
+                        <p className="text-white/50">On your FlightLogger main page, go to "My program".</p>
+                        <img src={Step1Guide2} className="object-cover rounded-lg mt-2" draggable="false"/>
+                    </div>
+                    <div className={"flex flex-col " + stepStyle}>
+                        <h3 className="text-lg font-semibold text-white">Step 2</h3>
+                        <p className="text-white/50">Select the flight you want to retrieve track data from.</p>
+                        <img src={Step2Guide2} className="object-cover rounded-lg mt-2" draggable="false"/>
+                    </div>
+                    <div className={"flex flex-col " + stepStyle}>
+                        <h3 className="text-lg font-semibold text-white">Step 3</h3>
+                         <p className="text-white/50">At the lesson page, go to "Flight".</p>
+                        <img src={Step3Guide2} className="object-cover rounded-lg mt-2" draggable="false"/>
+                    </div>
+                    <div className={"flex flex-col " + stepStyle}>
+                        <h3 className="text-lg font-semibold text-white">Step 4</h3>
+                         <p className="text-white/50">At "Flight Tracking" section, download the recording you want.</p>
+                        <img src={Step4Guide2} className="object-cover rounded-lg mt-2" draggable="false"/>
+                    </div>
+                    <div className={"flex flex-col " + stepStyle}>
+                        <h3 className="text-lg font-semibold text-white">Step 5</h3>
+                         <p className="text-white/50">On the logbook entry page, select "Add Flight Record".</p>
+                        <img src={Step5Guide2} className="object-cover rounded-lg mt-2" draggable="false"/>
+                    </div>
+                    <div className={"flex flex-col " + stepStyle}>
+                        <h3 className="text-lg font-semibold text-white">Step 6</h3>
+                         <p className="text-white/50">Select "AirNav Radar", upload the KML file then submit.</p>
+                        <img src={Step6Guide2} className="object-cover rounded-lg mt-2" draggable="false"/>
+                    </div>
+                </div>
+            </>
         },
         {
             id: "logbookreport",
