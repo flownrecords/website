51435ac93f22f52700febc4accb2df9deb46360b
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2025-09-22 13:20:11 +0100

    ✨ Add MapNotAvailable component to handle unavailable map state

---
+++ src/components/maping/MapNotAvail.tsx
@@ -0,0 +1,17 @@
+type ComponentProps = {
+    dimensions?: {
+        width?: string;
+        height?: string;
+    };
+}
+
+export default function MapNotAvailable({ dimensions } : ComponentProps) {
+    return (
+        <div
+            className="w-full overflow-hidden relative flex items-center justify-center"
+            style={{ height: dimensions?.height ?? "400px" }}
+        >
+            <h1 className="font-bold text-3xl text-white/15">Map not available</h1>
+        </div>
+    );
+}
