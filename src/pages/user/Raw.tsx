78b25ab943c93dda03b5071e9348928ad42f290d
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2025-09-24 19:51:11 +0100

    🚧 Added blockage to Raw.tsx

--- src/pages/user/Raw.tsx
+++ src/pages/user/Raw.tsx
@@ -1,33 +1,44 @@
 import { useAuth } from "../../components/auth/AuthContext";
 
 function syntaxHighlight(json: string) {
-  return json.replace(
-    /("(\\u[\da-fA-F]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(\.\d*)?([eE][+\-]?\d+)?)/g,
-    (match) => {
-      let cls = "number";
-      if (/^"/.test(match)) {
-        cls = /:$/.test(match) ? "key" : "string";
-      } else if (/true|false/.test(match)) {
-        cls = "boolean";
-      } else if (/null/.test(match)) {
-        cls = "null";
-      }
-      return `<span class="${cls}">${match}</span>`;
-    }
-  );
+    return json.replace(
+        /("(\\u[\da-fA-F]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(\.\d*)?([eE][+\-]?\d+)?)/g,
+        (match) => {
+        let cls = "number";
+        if (/^"/.test(match)) {
+            cls = /:$/.test(match) ? "key" : "string";
+        } else if (/true|false/.test(match)) {
+            cls = "boolean";
+        } else if (/null/.test(match)) {
+            cls = "null";
+        }
+        return `<span class="${cls}">${match}</span>`;
+        }
+    );
 }
 
 export default function Raw() {
-  const { user } = useAuth();
-  const { profilePictureUrl, ...rest } = user || {};
-  const parsed = { ...rest };
-  const json = JSON.stringify(parsed, null, 2);
+    const { user } = useAuth();
+    const { profilePictureUrl, passwordHash, ...rest } = user as any || {};
+    const parsed = { ...rest };
+    const json = JSON.stringify(parsed, null, 2);
 
-  return (
-    <pre
-      className="p-4 m-4 bg-secondary overflow-x-auto rounded-lg"
-      style={{ color: "#ccc" }}
-      dangerouslySetInnerHTML={{ __html: syntaxHighlight(json) }}
-    />
-  );
+    const PAGE_ENABLED = false;
+
+    return (
+        <>
+            {
+                user?.permissions?.includes("ADMIN") && PAGE_ENABLED ?
+                (
+                    <pre
+                    className="p-4 m-4 bg-secondary overflow-x-auto rounded-lg"
+                    style={{ color: "#ccc" }}
+                    dangerouslySetInnerHTML={{ __html: syntaxHighlight(json) }}
+                    />
+                ) : (
+                    <p className="text-center text-second-accent font-medium mt-10">You do not have permission to view this page.</p>
+                )
+            }
+        </>
+    );
 }
