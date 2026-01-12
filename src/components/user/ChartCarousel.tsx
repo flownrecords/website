ddd51b3b09ff1023934acc7914977a77002ed0b7
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2026-01-12 21:10:17 +0000

    ✏️ Added ACFT type fix for RTV: SR20

--- src/components/user/ChartCarousel.tsx
+++ src/components/user/ChartCarousel.tsx
@@ -52,6 +52,8 @@ function fixType(aircraftType?: string | null): string | undefined {
             type = "C172";
         } else if (type.startsWith("1")) {
             type = "C152";
+        } else if (type === "3S") {
+            type = "SR20";
         }
     }
 
